7773ed8077c9b4e2d28abdb8d9873774e9c7da1a
nemoeriksson <nemoeriksson2005@gmail.com>
2024-02-07 13:01:45 +0100

    Feat: Login & register different tabs

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,10 +1,40 @@
-<h1>Log in or sign up here (pls)</h1>
-<form method="POST" action="/login">
+<script lang="ts">
 
-	<label for="username">Username</label>
-	<input name="username" type="text">
-	<label for="password">Password</label>
-	<input name="password" type="password">
-	<input type="submit" name="login" value="log in" />
-	<input type="submit" name="register" value="register" />
-</form>
+enum Tabs {
+	LOGIN, 
+	REGISTER 
+}
+
+let tab = Tabs.LOGIN;
+</script>
+
+<div class="container">
+	{#if tab == Tabs.LOGIN}
+		<form action="?/login" method="post">
+			<div class="inputField">
+				<label for="user">Name or Email</label>
+				<input type="text" id="user" name="user">
+			</div>
+			<div class="inputField">
+				<label for="password-login">Password</label>
+				<input type="text" id="password-login" name="password">
+			</div>
+		</form>
+	{/if}
+	{#if tab == Tabs.REGISTER}
+		<form action="?/register" method="post">
+			<div class="inputField">
+				<label for="name">Name</label>
+				<input type="text" id="name" name="name"/>
+			</div>
+			<div class="inputField">
+				<label for="email">Email</label>
+				<input type="email" id="email" name="email">
+			</div>
+			<div class="inputField">
+				<label for="password-register">Password</label>
+				<input type="password" id="password-register" name="password">
+			</div>
+		</form>
+	{/if}
+</div>
