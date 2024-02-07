953dca9a90c44e23321cec142402dd118d40a3ab
Julius Rezai <microcapacitor2@gmail.com>
2024-02-07 12:08:27 +0100

    navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+
+<title>Fire site</title>
+
+<div id="site">
+	<nav>
+		<a id="name" href="/">OnFit</a>
+		<p class="seperator">|</p>
+		<section class="links">
+			<a href="/leaderboard">leaderboard</a>
+			<a href="/login">sign-up/sign-in</a>
+		</section>
+	</nav>
+	<slot/>
+</div>
+
+<style>
+	.links {
+		display: flex;
+		gap: 10px;
+	}
+	#site {
+		position: absolute;
+		left: 0;
+		top: 0;
+		margin: 0;
+	}
+	nav {
+		display: flex;
+		background-color: red;
+		margin: 0;
+		width: 100vw;
+	}
+	nav * {
+		font-size: 1rem;
+		padding: 0;
+		margin: 0;
+	}
+</style>
